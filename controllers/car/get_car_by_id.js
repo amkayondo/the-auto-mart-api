40cfb7ef5dff11d8bea28ee7204cb1e57e7af176
amkayondo <itskayondo01@gmail.com>
2019-06-12 11:29:27 -0700

    fix code

--- controllers/car/get_car_by_id.js
+++ controllers/car/get_car_by_id.js
@@ -2,14 +2,13 @@ const car_db = require('../../models/Cars');
 
 const get_car_by_id = (req, res) =>{
     const _req = parseInt(req.params.id); 
-    const found = car_db.some(
+ 
+    if (car_db.some(
         car => car.id === _req
-
-    );
-    
-    if (found) {
+    )) {
+        
         const _data = car_db.filter(
-            car => car.id === _req)
+        car => car.id === _req)
         res.json({
          status: 200,
          data : _data}   
