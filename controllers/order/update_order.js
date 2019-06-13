3854f4649b515b194430ccf0e6800c0dcb562dea
amkayondo <itskayondo01@gmail.com>
2019-06-13 09:46:44 -0700

    Add more models and config new routes for user and order

--- controllers/order/update_order.js
+++ controllers/order/update_order.js
@@ -5,7 +5,25 @@ const postOrder = (req, res) =>{
    const _order = parseInt(req.params.order_id);
    const got_order = order_db.some(x => x.id === _order);
    if(got_order){
-       
+       const updOrder = req.body;
+       order_db.forEach(order => {
+           if(order.id === _order){
+               order.price = req.body.price = updOrder.price
+               ? updOrder.price : order.price
+            
+                res.json({
+                    status: 200,
+                    message: "Order price updated",
+                    data: order
+                })
+            }
+
+       })
+   } else {
+       res.json({
+           status: 400,
+           error : `No order with id ${_order}`
+       })
    }
 
 }
