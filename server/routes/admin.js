44bd99f6707da96750db59e98806130d260de7ec
amkayondo <itskayondo01@gmail.com>
2019-06-21 13:35:53 +0200

    a

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import Auth from '../middleware/auth';
+import isAdmin from '../middleware/admin';
+
 import getAllCars from '../controllers/admin/get_all_cars';
 
 const adminRouter = express.Router();
 
 const auth = new Auth();
 
-adminRouter.get('/car', auth.auth, getAllCars);
+adminRouter.get('/car', [auth.auth, isAdmin], getAllCars);
 
 export default adminRouter;
