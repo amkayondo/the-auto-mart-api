803569491714b130977d2c2ec45f858a87a36c97
amkayondo <itskayondo01@gmail.com>
2019-06-22 09:55:43 +0200

    Modify app

--- server/index.js
+++ server/index.js
@@ -5,12 +5,15 @@ import carRouter from './routes/car';
 import Database from './controllers/db';
 import adminRouter from './routes/admin';
 
+const swaggerUI = require('swagger-ui-express');
+const swgui = require('../swagger.json');
+
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded());
-
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swgui));
 // index route
 app.get('/', (req, res) => {
   res.json({ message: 'Navigate to /api/v2 for v2' });
